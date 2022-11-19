b9d08ceebbcdeed72b74ce5bf4186eb427ca444c
KISHOR DHANGAR <111308956+kishordhangar@users.noreply.github.com>
2022-11-20 00:49:23 +0530

    20/11/2022

---
+++ 2_Hello_word_Using_Function.c
@@ -0,0 +1,21 @@
+//Header file in c(its an compulsory)
+
+#include<stdio.h>
+
+//entry point function
+int main()
+{
+
+    //call the function and  function declaration also
+    void Kishor();
+ 
+        Kishor();
+
+    return 0;
+}
+void Kishor()
+{
+
+printf("This is using function");
+
+}
--- Body.c
+++ Body.c
@@ -1,10 +1,9 @@
 
-
+// Body of c language coding
 #include<stdio.h>
 int main()
 {
 
-printf("\n\tc-programming batch November-2022\n\n");
 
     return 0;
 }
